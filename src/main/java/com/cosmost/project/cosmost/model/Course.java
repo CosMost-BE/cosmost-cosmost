72416f2fe8cc13e4fc86912ce102876ceb23305d
SeungHu Kim <shockim3710@naver.com>
2022-10-08 19:42:31 +0900

    [update] course model setting

--- src/main/java/com/cosmost/project/cosmost/model/Course.java
+++ src/main/java/com/cosmost/project/cosmost/model/Course.java
@@ -1,4 +1,32 @@
 package com.cosmost.project.cosmost.model;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 파라미터가 없는 생성자를 생성
+@AllArgsConstructor // 클래스에 존재하는 모든 필드에 대한 생성자를 자동으로 생성
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long authorId;
+
+    @NotNull
+    private String courseTitle;
+
+    @NotNull
+    private String courseComment;
+
+    @NotNull
+    private boolean courseStstus;
 }
